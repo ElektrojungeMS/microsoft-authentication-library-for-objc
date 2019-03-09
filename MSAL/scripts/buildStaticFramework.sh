7045ccbba7222cd45a6afc7539c6d4e394804ea9
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2019-03-08 21:43:19 -0800

    Clean up build script and target for static library

--- MSAL/scripts/buildStaticFramework.sh
+++ MSAL/scripts/buildStaticFramework.sh
@@ -2,16 +2,16 @@
 
 # Sets the target folders and the final framework product.
 FMK_NAME=MSAL
-TGT_NAME=${FMK_NAME}
+TGT_NAME="MSAL (Static iOS Framework)"
 
-echo "Building ${FMK_NAME} iOS static framework."
+echo "Building fat ${FMK_NAME} iOS static framework."
 
 # Install dir will be the final output to the framework.
 # The following line create it in the root folder of the current project.
-PRODUCTS_DIR=${SRCROOT}/../FooBar/iOS
+PRODUCTS_DIR="${SRCROOT}/MSAL Static Framework/iOS"
 
 # Working dir will be deleted after the framework creation.
-WRK_DIR=build
+WRK_DIR=${SRCROOT}/build
 DEVICE_DIR=${WRK_DIR}/Release-iphoneos
 SIMULATOR_DIR=${WRK_DIR}/Release-iphonesimulator
 
@@ -21,26 +21,30 @@ cd "${SRCROOT}"
 # Cleaning previous build.
 xcodebuild -project "${FMK_NAME}.xcodeproj" -configuration "Release" -target "${TGT_NAME}" clean
 
-# Building both architectures.
+# Building both device and simulator architectures.
 xcodebuild -project "${FMK_NAME}.xcodeproj" -configuration "Release" -target "${TGT_NAME}" -sdk iphoneos
 xcodebuild -project "${FMK_NAME}.xcodeproj" -configuration "Release" -target "${TGT_NAME}" -sdk iphonesimulator
 
-# Cleaning the oldest.
+# Cleaning the previous build.
 if [ -d "${PRODUCTS_DIR}" ]
 then
 rm -rf "${PRODUCTS_DIR}"
 fi
+mkdir -p "${PRODUCTS_DIR}"
 
-mkdir -p $PRODUCTS_DIR
+# Copy the device .framework to the final destination to preserve the structure.
+echo $DEVICE_DIR
+echo $PRODUCTS_DIR
+cp -R "$DEVICE_DIR/${FMK_NAME}.framework" "$PRODUCTS_DIR"
 
-# Copy the swift import file
-cp -R $DEVICE_DIR $PRODUCTS_DIR
+# Lipo it the device binary that was created with the simulator binary.
+LIB_IPHONEOS_FINAL="${PRODUCTS_DIR}/${FMK_NAME}.framework/${FMK_NAME}"
 
-# Create the arm64e slice in Xcode 10.1 and lipo it with the device binary that was created with oldest supported Xcode version.
-LIB_IPHONEOS_FINAL="${PRODUCTS_DIR}/Release-iphoneos/${FMK_NAME}.framework/${FMK_NAME}"
-#if [ -z "$MS_ARM64E_XCODE_PATH" ] || [ ! -d "$MS_ARM64E_XCODE_PATH" ]; then
-#echo "Environment variable MS_ARM64E_XCODE_PATH not set or not a valid path."
+# Remove the framework at our final location as it will only contain device architectures.
+rm -rf "$LIB_IPHONEOS_FINAL"
 
-echo "Use current Xcode version and lipo -create the fat binary."
-lipo -create "${LIB_IPHONEOS_FINAL}" "${SIMULATOR_DIR}/${FMK_NAME}.framework/${FMK_NAME}" -output "${PRODUCTS_DIR}/${FMK_NAME}"
+echo "Use lipo -create to create the fat binary."
+lipo -create "${DEVICE_DIR}/${FMK_NAME}.framework/${FMK_NAME}" "${SIMULATOR_DIR}/${FMK_NAME}.framework/${FMK_NAME}" -output "${LIB_IPHONEOS_FINAL}"
 
+# Delete the build directory
+rm -rf "${WRK_DIR}"
